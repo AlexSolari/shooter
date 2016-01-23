602cb4f98afc8b71ff45ec27c977dabec8f70f74
AlexSolari @ Laptop <alexsanify@gmail.com>
2016-01-23 21:13:56 +0000

    Added comments

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+/* --- Loading libraries --- */
 var http = require('http');
 var path = require('path');
 var socketio = require('socket.io');
 var express = require('express');
+/* --- Initializing server variables --- */
 var router = express();
 var server = http.createServer(router);
 var io = socketio.listen(server);
-
+/* --- Initializing server (disabling logging and setuping client-resourses resolving) --- */
 io.set('log level', 1);
 router.use(express.static(path.resolve(__dirname, 'client')));
-
+/* --- Starting server --- */
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("Server listening at", addr.address + ":" + addr.port);
