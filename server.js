80faabeb86a32e55a87e82f0b99489906276aedb
AlexSolari @ Laptop <alexsanify@gmail.com>
2016-01-23 20:54:43 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var path = require('path');
+var socketio = require('socket.io');
+var express = require('express');
+var router = express();
+var server = http.createServer(router);
+var io = socketio.listen(server);
+
+io.set('log level', 1);
+router.use(express.static(path.resolve(__dirname, 'client')));
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Server listening at", addr.address + ":" + addr.port);
+});
