7719810fed9a8c3b876e331686ee04115ccc434d
AlexSolari @ Laptop <alexsanify@gmail.com>
2016-01-24 12:36:09 +0000

    Fixed typo in Response handler method, and added username w/ default value for it

--- client/script/connection.js
+++ client/script/connection.js
@@ -1,12 +1,15 @@
 /*global io*/
-function Connection() {
+function Connection(name) {
+    this.username = name || "Anon";
+    
     this.socket = io.connect(document.location.href);
+    this.Send("login", this.username);
 }
 
 Connection.prototype.Send = function (type, data) {
     this.socket.emit(type, data);
 };
 
-Connection.prototype.AddRequestHandler = function (type, handler) {
+Connection.prototype.AddResponseHandler = function (type, handler) {
     this.socket.on(type, handler);
-}
+};
