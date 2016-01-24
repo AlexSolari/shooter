6e92ef016cf1f0f011a2ba9ea1e92558938ee979
AlexSolari @ Laptop <alexsanify@gmail.com>
2016-01-24 18:30:48 +0000

    Extended drawing class

--- client/script/drawing.js
+++ client/script/drawing.js
@@ -1,13 +1,77 @@
-function Drawer(){
-    this.Canvas = document.getElementById("canvas").getContext("2d");
+function ImagePool()
+{
+    this.data = [];    
 }
 
-Drawer.prototype.DrawText = function (text, x, y, size, font) {
-    x = x || 0;
-    y = y || 0;
-    size = size || 16;
-    font = font || "Arial";
+ImagePool.prototype.Get = function (url, callback) {
+    var result = this.data[url];
     
-    this.Canvas.font = size+"pt "+font;
-    this.Canvas.fillText(text,x,y);
+    if (!result)
+    {
+        var self = this;
+        result = new Image();
+        
+        result.onload = function () {
+            self.data[url] = result;
+            callback(result);
+        };
+        result.src = url;
+        
+        return;
+    }
+    
+    callback(result);
 };
+
+function Drawer() {
+    this.Pool = new ImagePool();
+    var domCanvas = document.getElementById("canvas");
+    domCanvas.width = window.innerWidth;
+    domCanvas.height = window.innerHeight;
+    this.Canvas = domCanvas.getContext("2d");
+}
+
+Drawer.prototype.Defaults = {
+        Font: "Arial",
+        FontSize: 16,
+        Color: "#000",
+        Angle: 0,
+    };
+
+Drawer.prototype.Text = function (text, x, y, size, font, color) {
+    size = size || this.Defaults.FontSize;
+    font = font || this.Defaults.Font;
+    color = color || this.Defaults.Color;
+    
+    this.Canvas.beginPath();
+    this.Canvas.font = size+"pt "+font;
+    this.Canvas.fillStyle = color;
+    this.Canvas.fillText(text,x,y);
+    this.Canvas.closePath();
+};
+
+Drawer.prototype.Circle = function(x, y, radius, color) {
+    color = color || this.Defaults.Color;
+    
+    this.Canvas.beginPath();
+    this.Canvas.arc(x,y,radius, 0, 2*Math.PI);
+    this.Canvas.fillStyle = color;
+    this.Canvas.fill();
+    this.Canvas.closePath();
+};
+
+Drawer.prototype.Image = function(url, x, y, angle) { 
+    angle = angle || this.Defaults.Angle;
+    
+    var self = this;
+    
+    this.Pool.Get(url, function (image) {
+        self.Canvas.save(); 
+	    self.Canvas.translate(x, y);
+	    self.Canvas.rotate(angle * Math.PI/180);
+	    self.Canvas.drawImage(image, -(image.width/2), -(image.height/2));
+	    self.Canvas.restore(); 
+    });
+    
+	
+}
--- client/script/script.js
+++ client/script/script.js
@@ -1,6 +1,9 @@
 /*global Connection*/
 /*global Drawer*/
 
+var drawer;
+
 window.onload = function() {
-  
+  drawer = new Drawer();
+  drawer.Image('http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg', 500, 500);
 };
