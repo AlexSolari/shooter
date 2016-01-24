dc74c9577b05c7ec74f4ad156b8ef911173d2f06
AlexSolari @ Laptop <alexsanify@gmail.com>
2016-01-24 12:25:09 +0000

    Moved socket.io event binding to server onstarted callback

--- server.js
+++ server.js
@@ -14,21 +14,19 @@ io.set('log level', 1);
 router.use(express.static(path.resolve(__dirname, 'client')));
 var sockets = [];
 
-io.on('connection', function (socket) {
-
-    sockets.push(socket);
-
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-    });
-
-    socket.emit("greeting", "Hello!");
-});
 
 /* --- Starting server --- */
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Server listening at", addr.address + ":" + addr.port);
+  io.on('connection', function (socket) {
+  
+      sockets.push(socket);
+  
+      socket.on('disconnect', function () {
+        sockets.splice(sockets.indexOf(socket), 1);
+      });
+  
+      socket.emit("greeting", "Hello!");
+  });
 });
 
 function broadcast(type, data){
