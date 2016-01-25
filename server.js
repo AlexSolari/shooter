4897dd7f55f4c55d5fdbeb00a5f9cee9188be45a
AlexSolari @ Laptop <alexsanify@gmail.com>
2016-01-25 14:59:42 +0000

    Rewrited server-code using wrappers

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var http = require('http');
 var path = require('path');
 var socketio = require('socket.io');
 var express = require('express');
+var game = require("game");
 
 /* --- Initializing server variables --- */
 var router = express();
@@ -12,23 +13,29 @@ var io = socketio.listen(server);
 /* --- Initializing server (disabling logging and setuping client-resourses resolving) --- */
 io.set('log level', 1);
 router.use(express.static(path.resolve(__dirname, 'client')));
-var sockets = [];
-
+var UpdatesPerSecond = 60;
 
 /* --- Starting server --- */
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   io.on('connection', function (socket) {
-  
-      sockets.push(socket);
-  
-      socket.on('disconnect', function () {
-        sockets.splice(sockets.indexOf(socket), 1);
+      
+    var client = game.AddClient(socket);
+        
+    client.AddRequestHandler('disconnect', function () {
+        game.RemoveClient(client.id);
+    });
+     
+    client.AddRequestHandler("getInput_response", function(data) {
+        client.input = data;
       });
+      
   });
+  
+  setInterval(function () {
+      
+    game.Broadcast("getInput_request");
+    game.Broadcast("gamestate", game.GetState());
+    
+  }, 1000 / UpdatesPerSecond);
 });
 
-server.broadcast = function(type, data){
- sockets.forEach(function (socket) {
-   socket.emit(type, data);
- }); 
-};
